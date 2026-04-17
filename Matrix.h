82d0f4e36f31cf1ae6082eb557d11fdf655ab151
hanapulsar <217953127+hanapulsar@users.noreply.github.com>
2026-04-17 19:51:31 +0900

    Initial commit - copied LAB_2 as base

---
+++ Matrix.h
@@ -0,0 +1,181 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#include <iostream>
+#include <stdexcept>
+#include <omp.h>
+
+template <typename T>
+class Matrix {
+private:
+	size_t size;
+	T* data = nullptr;
+
+public:
+	Matrix();
+	Matrix(size_t size);
+	~Matrix();
+	Matrix(const Matrix& src);
+	Matrix& operator=(const Matrix& src);
+
+	T& operator()(size_t row, size_t column);
+	const T& operator()(size_t row, size_t column) const;
+
+	Matrix operator*(const Matrix& src) const;
+	Matrix multiply_omp(const Matrix& src, int num_threads) const;
+
+	size_t get_size() const;
+};
+
+//Default constructor
+template <typename T>
+Matrix<T>::Matrix() : size(0) {
+}
+
+//Constructor
+template <typename T>
+Matrix<T>::Matrix(size_t size) : size(size) {
+	if (size == 0) {
+		data = nullptr;
+		return;
+	}
+
+	data = new T[size * size]();
+}
+
+//Destructor
+template <typename T>
+Matrix<T>::~Matrix() {
+	delete[] data;
+}
+
+//Copy constructor
+template <typename T>
+Matrix<T>::Matrix(const Matrix& src) : size(src.size) {
+	if (size > 0) {
+		data = new T[size * size];
+		for (size_t i = 0; i < size * size; ++i) {
+			data[i] = src.data[i];
+		}
+	}
+}
+
+//Assigment operator
+template <typename T>
+Matrix<T>& Matrix<T>::operator=(const Matrix& src) {
+	if (this == &src) {
+		return *this;
+	}
+
+	delete[] data;
+
+	size = src.size;
+
+	if (size > 0) {
+		data = new T[size * size];
+		for (size_t i = 0; i < size * size; ++i) {
+			data[i] = src.data[i];
+		}
+	}
+	else {
+		data = nullptr;
+	}
+
+	return *this;
+}
+
+
+//Functor operator (write)
+template <typename T>
+T& Matrix<T>::operator()(size_t row, size_t column) {
+	if (row >= size || column >= size) {
+		throw std::out_of_range("Index out of range.");
+	}
+
+	return data[row * size + column];
+}
+
+
+//Functor operator const (read)
+template <typename T>
+const T& Matrix<T>::operator()(size_t row, size_t column) const {
+	if (row >= size || column >= size) {
+		throw std::out_of_range("Index out of range.");
+	}
+
+	return data[row * size + column];
+}
+
+//Multiplication operator
+template <typename T>
+Matrix<T> Matrix<T>::operator*(const Matrix& src) const {
+	if (size != src.size) {
+		throw std::invalid_argument("Matrix dimensions different.");
+	}
+
+	Matrix<T> result(size);
+
+	for (size_t i = 0; i < size; ++i) {
+		for (size_t j = 0; j < size; ++j) {
+			T sum = 0;
+			for (size_t k = 0; k < size; ++k) {
+				sum += (*this)(i, k) * src(k, j);
+			}
+			result(i, j) = sum;
+		}
+	}
+
+	return result;
+}
+
+//Parallel multiplication
+template <typename T>
+Matrix<T> Matrix<T>::multiply_omp(const Matrix& src, int num_threads) const {
+	if (size != src.size) {
+		throw std::invalid_argument("Matrix dimensions different.");
+	}
+
+	Matrix<T> result(size);
+
+	omp_set_num_threads(num_threads);
+
+	// error C3016: 'i': index variable in OpenMP 'for' statement must have signed integral type
+	int n = static_cast<int>(size);
+
+	#pragma omp parallel for
+	for (int i = 0; i < n; ++i) {
+		for (size_t j = 0; j < size; ++j) {
+			T sum = 0;
+			for (size_t k = 0; k < size; ++k) {
+				sum += (*this)(i, k) * src(k, j);
+			}
+			result(i, j) = sum;
+		}
+	}
+
+	return result;
+}
+
+//Getter
+template <typename T>
+size_t Matrix<T>::get_size() const {
+	return size;
+}
+
+//Output operator
+template <typename T>
+std::ostream& operator<<(std::ostream& ostream, const Matrix<T>& src) {
+	size_t N = src.get_size();
+	for (size_t i = 0; i < N; ++i) {
+		for (size_t j = 0; j < N; ++j) {
+			ostream << src(i, j);
+			if (j < N - 1) {
+				ostream << " ";
+			}
+		}
+		ostream << "\n";
+	}
+	return ostream;
+}
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,110 @@
+#include <random>
+#include <fstream>
+#include <chrono>
+//#include <iomanip>
+#include "Matrix.h"
+
+using namespace std;
+
+//Generate random square matrix
+static Matrix<double> random_matrix(size_t n) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<double> dist(-10.0, 10.0);
+
+    Matrix<double> m(n);
+    for (size_t i = 0; i < n; i++) {
+        for (size_t j = 0; j < n; j++) {
+            m(i, j) = dist(gen);
+        }
+    }
+    return m;
+}
+
+/*
+//Read matrix from file
+static Matrix<double> read_matrix(const string& filepath) {
+    ifstream input(filepath);
+    if (!input.is_open()) {
+        throw runtime_error("Can't open: " + filepath);
+    }
+    size_t n;
+    input >> n;
+    Matrix<double> matrix(n);
+    for (size_t i = 0; i < n; i++) {
+        for (size_t j = 0; j < n; j++) {
+            input >> matrix(i, j);
+        }
+    }
+    return matrix;
+}
+*/
+
+//Write matrix from file
+static void write_matrix(const string& filepath, const Matrix<double>& matrix) {
+    ofstream output(filepath);
+    if (!output.is_open()) {
+        throw runtime_error("Can't open: " + filepath);
+    }
+    output << matrix.get_size() << "\n";
+
+    //Need this if not using rounding in .py for comparison
+    //output << std::setprecision(17) << matrix;
+
+    output << matrix;
+}
+
+int main() {
+    vector<int> sizes = { 200, 400, 800, 1200, 1600, 2000 };
+    vector<int> threads = { 1, 2, 4, 8 };
+
+    try {
+        Matrix<double> A, B, C;
+
+        for (int size : sizes) {
+            cout << "\nGenerating random matrices. Size: " << size << "x" << size << "\n\n";
+            A = random_matrix(size);
+            B = random_matrix(size);
+
+            double time_1_thread = 0.0;
+
+            for (int t : threads) {
+                auto start = chrono::high_resolution_clock::now();
+                C = A.multiply_omp(B, t);
+                auto end = chrono::high_resolution_clock::now();
+
+                double elapsed_sec = chrono::duration<double>(end - start).count();
+
+                double acceleration = 1.0;
+                if (t == 1) {
+                    time_1_thread = elapsed_sec;
+                }
+                else {
+                    acceleration = time_1_thread / elapsed_sec;
+                }
+
+                double efficiency = acceleration / t;
+
+                cout << "Size: " << size << "\n";
+                cout << "Threads: " << t << "\n";
+                cout << "Elapsed time: " << elapsed_sec << "\n";
+                cout << "Accelaration: " << acceleration << "\n";
+                cout << "Efficiency: " << efficiency << "\n";
+
+                if (size == 1200 && t == 4) {
+                    cout << "Saving sample for verifing, size: 1200*1200 for 4 threads.\n";
+                    write_matrix("InputA.txt", A);
+                    write_matrix("InputB.txt", B);
+                    write_matrix("Output.txt", C);
+                }
+            }
+        }
+        cout << "Finished.\n";
+    }
+    catch (const exception& e) {
+        cerr << "Error: " << e.what() << "\n";
+        return -1;
+    }
+
+    return 0;
+}
